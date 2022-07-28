c3b3baa125e685b34155ffab620bbc6642ad59b4
spykelion <ndilionel6@gmail.com>
2022-07-28 18:35:31 +0100

    create setup scripts

--- backend/library/management/commands/setup.py
+++ backend/library/management/commands/setup.py
@@ -4,20 +4,25 @@ import subprocess
 
 
 class Command(BaseCommand):
-    help = 'Automate the setup'
+    help = 'Install required files and create venv'
 
     def handle(self, *args, **kwargs):
         upgrade_pip_v = ["python", "-m", "pip", "install", "--upgrade", "pip"]
         install_dependencies = ["pip", "install",
                                 "-r", "requirements.txt"]
         create_venv = ["python", "-m", "venv", "env"]
-        activate_venv = ["env\\scripts\\activate"]
-        cmds = [upgrade_pip_v, install_dependencies,
-                create_venv, activate_venv]
+        cmds = [upgrade_pip_v, install_dependencies]
+                
         for i in range(len(cmds)-1):
             try:
                 subprocess.run(cmds[i], check=True)
             except Exception as e:
-                print("Error. Check your network..", e)
+                print("Error..", e)
             finally:
                 continue
+        print("Succesfully Installed all requirements.")
+        print("Creating virtual environment....")
+        subprocess.run(create_venv, check=True)
+        print("Finished Creating the virtual environment.")
+        print("Run the cmd below to activate venv\n")
+        print("env\\Scripts\\activate")
