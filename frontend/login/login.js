40ca90176e51eb54c1ab094f6256b194c89aad8e
spykelion <ndilionel6@gmail.com>
2022-08-03 16:51:53 +0100

    fix image size:illustration

--- frontend/login/login.js
+++ frontend/login/login.js
@@ -1,6 +1,7 @@
 let eye_open = document.getElementById("eye_open")
 let eye_close = document.getElementById("eye_close")
 let password = document.getElementById("password")
+let illustration = document.getElementById("illustration")
 
 eye_open.addEventListener("click", function(e){
     eye_close.classList.add("show")
@@ -17,3 +18,16 @@ eye_close.addEventListener("click", function(e){
     password.type = "password"
     // eye_close.classList.remove("show")
 })
+
+function detectBrowser() {
+    if (navigator.userAgent.includes("Chrome")) {
+      return "chrome"
+    }
+    if (navigator.userAgent.includes("Firefox")) {
+      return "firefox"
+    }
+    if (navigator.userAgent.includes("Safari")) {
+      return "safari"
+    }
+  }
+  illustration.classList.add(detectBrowser())
