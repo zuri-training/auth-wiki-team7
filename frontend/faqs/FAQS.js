62f6951c396c28076820091fac22933dd5f0a786
Suksman03 <ndirmbulasuki@gmail.com>
2022-08-07 01:48:15 -1100

    FAQ's page

---
+++ frontend/faqs/FAQS.js
@@ -0,0 +1,26 @@
+let menuBtn = document.querySelector(".menu_btn");
+let navCard = document.querySelector(".nav_card");
+
+menuBtn.addEventListener("click", () => {
+    navCard.classList.toggle("hide");
+})
+document.getElementById("form").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+});
+document.getElementById("form1").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+});
+document.getElementById("form2").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+});
+document.getElementById("form3").addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log(e);
+});
+ 
+ 
+ 
+ 
