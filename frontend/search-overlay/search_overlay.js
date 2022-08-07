144bb46c117378182cccfd2652ba45411447a47c
Adeola-creator <59116131+Adeola-creator@users.noreply.github.com>
2022-08-07 11:33:09 +0100

    search bar and search icon (#52)
    * search overlay first attempt
    
    * search box adjusted
    
    * grid display
    
    * high-fi colors
    
    * fix search_overlay
    > fix icons
    > conditional display (DOM appearance)
    
    Co-authored-by: NdI Lionel <59184392+spykelionel@users.noreply.github.com>
    Co-authored-by: spykelion <ndilionel6@gmail.com>

---
+++ frontend/search-overlay/search_overlay.js
@@ -0,0 +1,6 @@
+let _so_main = document.getElementById("so_main");
+let _close = document.getElementById("so_close_so");
+
+_close.addEventListener("click", function(e){
+    _so_main.style.display = 'none';
+})
