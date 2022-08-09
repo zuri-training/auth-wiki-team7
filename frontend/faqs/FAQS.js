5ace0bba230e9b43c4d84d71225a674294abbee0
Suksman03 <ndirmbulasuki@gmail.com>
2022-08-09 09:35:32 -1100

    Corrections

--- frontend/faqs/FAQS.js
+++ frontend/faqs/FAQS.js
@@ -4,19 +4,19 @@ let navCard = document.querySelector(".nav_card");
 menuBtn.addEventListener("click", () => {
     navCard.classList.toggle("hide");
 })
-document.getElementById("form").addEventListener("submit", (e) => {
+document.getElementById("faq-form").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
 });
-document.getElementById("form1").addEventListener("submit", (e) => {
+document.getElementById("faq-form1").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
 });
-document.getElementById("form2").addEventListener("submit", (e) => {
+document.getElementById("faq-form2").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
 });
-document.getElementById("form3").addEventListener("submit", (e) => {
+document.getElementById("faq-form3").addEventListener("submit", (e) => {
     e.preventDefault();
     console.log(e);
 });
