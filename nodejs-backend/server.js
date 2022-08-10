0d76e59f0aeef62c76f89bbd96d6db09e1c7d926
spykelion <ndilionel6@gmail.com>
2022-08-10 16:37:18 +0100

    Create signup and documentation page

--- nodejs-backend/server.js
+++ nodejs-backend/server.js
@@ -6,21 +6,7 @@ var app = express();
 app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
-// use res.render to load up an ejs view file
-let pets = [
-  {
-    name: "Meowsalot",
-    description: "From the tiny Rusty-spotted cat of Sri Lanka to the massive Siberian tiger of the Russian Far East, there are 40 species of wild cats in the world, and each of them is as beautiful as it is unique.Most of us know lions, tigers, jaguars and leopards, but what are all the other types of wild cats out there? If you consider yourself a cat person or simply curious about these charismatic animals, read on to meet the family. They are the most loved and cute species found on the land.",
-    species: "cat",
-    image: "https://540934-1733880-raikfcquaxqncofqfm.stackpathdns.com/wp-content/uploads/2020/05/Eurasian-lynx-AdobeStock_246336076-1024x683.jpg",
-  },
-  {
-    name: "Barksalot",
-    description: "Dogs are the most variable mammal on earth with around 450 globally recognized dog breeds.[10] In the Victorian era, directed human selection developed the modern dog breeds, which resulted in a vast range of phenotypes. Most breeds were derived from small numbers of founders within the last 200 years, and since then dogs have undergone rapid phenotypic change and were formed into today's modern breeds due to artificial selection imposed by humans.",
-    species: "dog",
-    image: "https://upload.wikimedia.org/wikipedia/commons/e/e0/Dog_niemiecki_%C5%BC%C3%B3%C5%82ty_LM980.jpg",
-  }
-];
+
 // index pages
 let dir = "";
 app.get("/", function (req, res) {
@@ -29,13 +15,28 @@ app.get("/", function (req, res) {
 
 // about page
 app.get("/about", function (req, res) {
-  res.render("pages/about", {pets});
+  res.render("pages/about");
 });
 // login page
 app.get("/login", function (req, res) {
   res.render("pages/login",);
 });
-
+// signup page
+app.get("/signup", function (req, res) {
+  res.render("pages/signup",);
+});
+// product page
+app.get("/products", function (req, res) {
+  res.render("pages/product",);
+});
+// documentation page
+app.get("/documentation", function (req, res) {
+  res.render("pages/documentation",);
+});
+// Error page
+app.get("*", function (req, res) {
+  res.render("pages/error",);
+});
 
 app.listen(8080);
 console.clear();
