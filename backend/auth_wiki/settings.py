f02047d4eb7410088c2ab0af8784e6353fa01416
Echefu Louis <55154145+echefulouis@users.noreply.github.com>
2022-08-11 08:33:56 +0100

    Bn 404 (#161)
    * landing page fix
    
    * Fix conflict:
    Restructure pages.
    
    * landing page update
    
    * redisigned landing page
    
    * landing page update
    
    * new update
    
    * change button color
    
    * fix style issues
    
    * monday update
    
    * added the remaining images at the background, re-aligned the middle2 div, re-arranged the folder structure
    
    * removed all header and footer from html and css
    
    * recent integration
    
    * latest changes
    
    * Implement 404
    
    Co-authored-by: Labeeb Adegoke <77116237+Lharbeeb@users.noreply.github.com>
    Co-authored-by: spykelion <ndilionel6@gmail.com>
    Co-authored-by: NdI Lionel <59184392+spykelionel@users.noreply.github.com>
    Co-authored-by: Adewale-alt <adebimpehabeeb10@gmail.com>

--- backend/auth_wiki/settings.py
+++ backend/auth_wiki/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&bbf(+np&$eirxezv2xapvzd2si-vj6_%dxw9)q4+^e_4pdhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
