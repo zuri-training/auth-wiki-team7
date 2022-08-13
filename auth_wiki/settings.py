6bb2087ff285b17345bf5b23e42f66ea9711a92c
Adewale-alt <adebimpehabeeb10@gmail.com>
2022-08-13 04:54:06 +0100

    latest_commit

--- auth_wiki/settings.py
+++ auth_wiki/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&bbf(+np&$eirxezv2xapvzd2si-vj6_%dxw9)q4+^e_4pdhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
