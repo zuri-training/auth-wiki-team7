981cee0f915e6f471707e2415a1e06dcd0e6b930
Adewale-alt <adebimpehabeeb10@gmail.com>
2022-08-13 04:18:34 +0100

    latest_commit

--- auth_wiki/settings.py
+++ auth_wiki/settings.py
@@ -29,9 +29,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9&bbf(+np&$eirxezv2xapvzd2si-vj6_%dxw9)q4+^e_4pdhj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://auth-team7.herokuapp.com/']
 
 
 # Application definition
